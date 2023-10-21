2b589d3484bc134bbaf13b6f09a36812268673a1
felixpincher <felixpincher@gmail.com>
2023-10-21 15:41:26 +0200

    commit -m "added repositories and interfaces and implementetion of one of the interfaces"

---
+++ TestTask_MVP_DreamDriven.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask_MVP_DreamDriven.DataAccess.Data.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOfDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity);
+
+        void Remove(int id);
+
+        void Remove(T entity); // to remove from database
+
+    }
+}
--- TestTask_MVP_DreamDriven.DataAccess/Data/Repository/Repository.cs
+++ TestTask_MVP_DreamDriven.DataAccess/Data/Repository/Repository.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using TestTask_MVP_DreamDriven.DataAccess.Data.Repository.IRepository;
 
 namespace TestTask_MVP_DreamDriven.DataAccess.Data.Repository
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext Context;
+        internal DbSet<T> dbSet;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOfDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = dbSet.Find(id);
+            Remove(entityToRemove);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
     }
 }
