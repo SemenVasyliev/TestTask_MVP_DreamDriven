bbf2dcf843c8d9f561685c943cbbe7e4a8f4ac00
felixpincher <felixpincher@gmail.com>
2023-10-21 23:57:03 +0200

    commit -m "added a CategoryPage with table. added dropdown menu at home page"

---
+++ TestTask_MVP_DreamDriven/Pages/Admin/Category/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestTask_MVP_DreamDriven.Pages.Admin.Category
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
