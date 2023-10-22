a3929a400ebc902126d4e4b2dcb9257dd159a75c
felixpincher <felixpincher@gmail.com>
2023-10-22 14:54:35 +0200

    commit -m "finished Delete. added notifications"

--- TestTask_MVP_DreamDriven/Controllers/CategoryController.cs
+++ TestTask_MVP_DreamDriven/Controllers/CategoryController.cs
@@ -20,5 +20,18 @@ namespace TestTask_MVP_DreamDriven.Controllers
         {
             return Json(new { data = _unitOfWork.Category.GetAll() });
         }
+
+        [HttpDelete("{id}")]
+         public IActionResult Delete(int id) 
+        {
+            var ObjFromDb = _unitOfWork.Category.GetFirstOfDefault(u=>u.Id == id);
+            if (ObjFromDb == null) 
+            {
+                return Json(new { success= false, message="Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(ObjFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful" });
+        }
     }
 }
